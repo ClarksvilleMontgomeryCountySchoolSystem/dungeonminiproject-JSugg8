bd7571b6a096e3fd06ba8d7a734bfdebded3852f
JSugg <jonah.sugg821@students.cmcss.net>
2026-03-05 10:56:12 -0600

    ejfih

--- task2.py
+++ task2.py
@@ -1,6 +1,25 @@
 has_key= True
+good=r'''  ___________ @ @
+          /         (@\   @
+          \___________/  _@
+                    @  _/@ \_____
+                     @/ \__/-="="`
+                      \_ /
+                       <|
+                       <|
+              jgs      <|'''
+bad=r""" ,,,,,,,,,,,,,,,,,,,,
+&&&&&&&&&&&&&&&&&&&&/
+&&&&&&&M|" "|M&&&&&&/
+&&&&&&M|     |M&&&&&/ '94-the wolfe
+&&&&&&&M|   |M&&&&&&/
+&&&&&&&&M| |M&&&&&&&/
+&&&&&&&&M\_/M&&&&&&&/
+&&&&&&&&&&&&&&&&&&&&/"""
 if has_key:
     outcome="Click: You can unlock the door"
+    print(good)
 else:
     outcome = "Doom: You are locked out"
+    print(bad)
 print(outcome)
